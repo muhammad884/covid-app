62bcc4d1eed18e15f3c57031fcf69d464eef7cc4
muhammad884 <muhammadullah403@gmail.com>
2022-06-13 12:00:55 +0500

    refresh problem solving

--- backend/app.js
+++ backend/app.js
@@ -36,9 +36,9 @@ app.use("/covid/person", PersonRouter);
 app.use("/covid/person/travel", PersonTravelRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "clent/build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
 
