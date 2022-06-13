180c96edda4177c8adb95fc27bd910fc0f03a0f4
muhammad884 <muhammadullah403@gmail.com>
2022-06-13 11:44:15 +0500

    solve refresh problem

--- backend/app.js
+++ backend/app.js
@@ -35,25 +35,25 @@ app.use("/covid/person", PersonRouter);
 // person travel routes
 app.use("/covid/person/travel", PersonTravelRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Please use the production server");
-  });
-}
-
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("frontend/build"));
-//   app.get("/*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Please use the production server");
 //   });
 // }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"));
+  });
+}
 
 app.use(errorHandler);
 app.listen(port, () => {
